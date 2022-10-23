dbba35da1b9b76b3dfacb1ded0f5304a2843c037
Vitaly Chashin <wchfgpl@gmail.com>
2022-10-23 15:28:17 +0300

    Basically works

---
+++ GlobalProtectMission/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace GlobalProtectMission
+{
+    public class Main
+    {
+        
+        public static bool enabled;
+
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            TIMissionEffect_GlobalProtect tim = new TIMissionEffect_GlobalProtect(); //load it
+
+            return true;
+        }
+    }
+}
---
+++ GlobalProtectMission/TIMissionEffect_GlobalProtect.cs
@@ -0,0 +1,44 @@
+﻿
+    using PavonisInteractive.TerraInvicta;
+    using UnityEngine;
+
+    public class TIMissionEffect_GlobalProtect:TIMissionEffect
+    {
+        public override string ApplyEffect(TIMissionState mission, TIGameState target, TIMissionOutcome outcome = TIMissionOutcome.Success)
+        {
+            
+
+           
+            string result = "";
+
+            string pointDate = "";
+
+            int pointCount = 0;
+
+            foreach (var factionControlPoint in mission.councilor.faction.controlPoints)
+            {
+                pointDate= factionControlPoint.ResolveDefendControlPointEffect(6);
+                pointCount++;
+            }
+
+            string habDate = "";
+            int habCount = 0;
+
+            foreach (var factionHab in mission.councilor.faction.habs)
+            {
+                habDate= factionHab.ResolveDefendHabEffect(mission.councilor.faction, 12);
+                habCount++;
+            }
+
+            result = Loc.T("TIMissionTemplate.GlobalProtect.pointProtect") + " " + pointCount + " " +
+                     Loc.T("TIMissionTemplate.GlobalProtect.till") + " " + pointDate;
+
+            if (habCount > 0)
+            {
+                result = result + "\n" + Loc.T("TIMissionTemplate.GlobalProtect.habProtect") + " " + pointCount + " " +
+                         Loc.T("TIMissionTemplate.GlobalProtect.till") + " " + habDate;
+            }
+
+            return result;
+        }
+    }
